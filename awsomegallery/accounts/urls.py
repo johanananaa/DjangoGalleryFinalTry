fdd8dcc0e3de195257bc3b90f91f0fb6250af59c
johanananaa <johanna.mersmann@hs-augsburg.de>
2025-07-16 14:12:48 +0200

    Implement user registration.

--- awsomegallery/accounts/urls.py
+++ awsomegallery/accounts/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
-    #path('register/', views.registerView, name="register_url"),
+    path('register/', views.register, name="register_url"),
     path('logout/', LogoutView.as_view(), name="logout"),
 ]
