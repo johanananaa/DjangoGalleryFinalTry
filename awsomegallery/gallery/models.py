6ac865e53721dd604d904cbf0c8df1bafde99f6a
johanananaa <johanna.mersmann@hs-augsburg.de>
2025-07-16 14:28:54 +0200

    Adde user reference to images. Show only the images of the currently logged-in user on the overview page.

--- awsomegallery/gallery/models.py
+++ awsomegallery/gallery/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class Image(models.Model):
     file = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- awsomegallery/gallery/views.py
+++ awsomegallery/gallery/views.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from gallery.models import Image
@@ -9,19 +10,23 @@ from gallery.models import Image
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = []
+        exclude = ['user']
 
 def overview(request):
-    all_images = Image.objects.all()
+    if request.user.is_authenticated:
+        all_images = Image.objects.filter(user=request.user).all()
+    else:
+        all_images = Image.objects.all()
     return render(request, 'gallery/overview.html',
             {'images': all_images})
 
-
+@login_required
 def upload(request):
     if request.method == 'POST':
         #actually upload the image
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
+            form.instance.user = request.user #set the user to the currently logged-in user
             form.save()
             return redirect('overview')
     else:
